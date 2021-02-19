67c9d1820cb714231fb6b88a26d5c2a4add92d6b
Code JSON <73917308+codragon2020@users.noreply.github.com>
2021-02-18 21:38:23 -0500

    Updated with seeds data and viewAllEmployees function

--- app.js
+++ app.js
@@ -49,7 +49,81 @@ function startApp(){
         "View department budgets"
       ]
     })
-    .then(() => {
-        connection.end();
-    })
+    .then((answer) => {
+
+        // Switch case depending on user option
+        switch (answer.action) {
+            
+            case "View all employees":
+                viewAllEmp();
+                break;
+
+            // case "View all employees by department":
+            //     viewAllEmpByDept();
+            //     break;
+
+            // case "View all employees by role":
+            //     viewAllEmpByRole();
+            //     break;
+
+            // case "Add employee":
+            //     addEmp();
+            //     break;
+
+            // case "Add department":
+            //     addDept();
+            //     break;
+
+            // case "Add role":
+            //     addRole();
+            //     break;
+
+            // case "Update employee role":
+            //     updateEmpRole();
+            //     break;
+
+            // case "Update employee manager":
+            //     updateEmpMngr();
+            //     break;
+
+            // case "View all employees by manager":
+            //     viewAllEmpByMngr();
+            //     break;
+
+            // case "Delete employee":
+            //     deleteEmp();
+            //     break;
+
+            // case "View department budgets":
+            //     viewDeptBudget();
+            //     break;
+
+            // case "Delete role":
+            //     deleteRole();
+            //     break;
+
+            // case "Delete department":
+            //     deleteDept();
+            //     break;
+        }
+    });
+}
+
+// View all employees 
+function viewAllEmp(){
+
+    // Query to view all employees
+    let query = "SELECT e.id, e.first_name, e.last_name, role.title, department.name AS department, role.salary, concat(m.first_name, ' ' ,  m.last_name) AS manager FROM employee e LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN role ON e.role_id = role.id INNER JOIN department ON role.department_id = department.id ORDER BY ID ASC";
+
+    // Query from connection
+    connection.query(query, function(err, res) {
+        if(err) return err;
+        console.log("\n");
+
+        // Display query results using console.table
+        console.table(res);
+
+        //Back to main menu
+        startApp();
+    });
 }
---
+++ seeds.sql
@@ -0,0 +1,77 @@
+USE employees_DB;
+
+-- Department Seeds
+
+INSERT INTO department (id, name)
+VALUES (1, "Tech");
+
+INSERT INTO department (id, name)
+VALUES (2, "Brand");
+
+INSERT INTO department (id, name)
+VALUES (3, "HR");
+
+INSERT INTO department (id, name)
+VALUES (4, "Product");
+
+-- Role Seeds
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (1, "Engineer", 120000, 1);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (2, "Sr Engineer", 140000, 1);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (3, "IT Manager", 160000, 1);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (4, "Brand Manager", 95000, 2);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (5, "HR Consultant", 85000, 3);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (6, "HR Manager", 110000, 3);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (7, "Product Analyst", 80000, 4);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (8, "Product Owner", 105000, 4);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (9, "Product Manager", 135000, 4);
+
+-- Employees Seeds
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (4, "Louis", "Lawrence", 3, null);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (5, "Isaac", "Havoc", 4, null);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (7, "Bennie", "Raine", 6, null);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (10, "Zach", "Skye", 9, null);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (3, "Matthew", "Christy", 2, 3);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (1, "Alec", "Mars", 1, 3);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (2, "Bert", "Rider", 1, 3);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (6, "Gerard", "Cotton", 5, 7);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (8, "Benjamin", "Jason", 7, 10);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (9, "Bryan", "Gates", 8, 10);
+
